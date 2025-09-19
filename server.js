121fac4459a310c576a240d842f93bc6562e7b99
azuh salvation <salvationazuh@gmail.com>
2025-09-19 21:46:18 +0100

    update

--- server.js
+++ server.js
@@ -12,5 +12,5 @@ app.use("/", email);
 const PORT = 5000;
 
 app.listen(PORT, () => {
-  console.log("server is running on http://localhost:${PORT}");
+  console.log(`server is running on http://localhost:${PORT}`);
 });
